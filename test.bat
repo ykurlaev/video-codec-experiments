65b307a1d553c4bf5b8b678df458745a1e5f5ebd
Yaroslav Kurlaev <yaroslav.kurlaev@gmail.com>
2013-08-16 19:07:33 +0700

    Updated test data

--- test.bat
+++ test.bat
@@ -1,13 +1,13 @@
 @echo off
 for /l %%i in (1, 1, 100) do (
-    Release\Codec.exe -e test.yuv 640 346 test.compressed %%i 0 -s
-    Release\Codec.exe -d test.compressed out.yuv %%i 0 -s
+    Release\Codec.exe -e test.yuv 640 346 test.compressed %%i 0 -q
+    Release\Codec.exe -d test.compressed out.yuv -q
     Release\PSNR.exe 640 346 test.yuv out.yuv
     for %%F in (test.compressed) do echo %%~zF
 )
 for /l %%i in (1, 1, 255) do (
-    Release\Codec.exe -e test.yuv 640 346 test.compressed %%i 1 -s
-    Release\Codec.exe -d test.compressed out.yuv %%i 1 -s
+    Release\Codec.exe -e test.yuv 640 346 test.compressed %%i 1 -q
+    Release\Codec.exe -d test.compressed out.yuv -q
     Release\PSNR.exe 640 346 test.yuv out.yuv
     for %%F in (test.compressed) do echo %%~zF
 )
